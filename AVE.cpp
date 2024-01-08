7f86c41284d6b1f7eabdadd3e2a9fe7355258b90
tomaszZuranski <tomasz.zuranski87@gmail.com>
2024-01-08 15:59:30 +0100

    Removed unsuported image formats from SDL_image initialization.

--- AVE.cpp
+++ AVE.cpp
@@ -11,7 +11,7 @@ namespace AVE
             return true;
         int error_code;
         if((error_code = SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS))==0)
-            if(IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG|IMG_INIT_TIF|IMG_INIT_WEBP|IMG_INIT_JXL|IMG_INIT_AVIF)==0)
+            if(IMG_Init(IMG_INIT_JPG|IMG_INIT_PNG|IMG_INIT_WEBP)==0)
             {
                 std::clog << "Initialized AVE" << std::endl;
                 return true;
