8fbad069a0a5b20dfa26e2babd11c17977f4f636
tomaszZuranski <345322@uwr.edu.pl>
2024-01-08 17:41:44 +0100

    created two new build targets for client and server. renamed previous main.cpp to testing.cpp to make it more accurate with what it really is

--- testing.cpp
+++ testing.cpp
@@ -1,23 +1,19 @@
 #include "gameGUI.hpp"
-#include <cstdio>
 int main(int argc, const char *argv[])
 {
     int windowWidth = 1280, windowHeight = 960;
     int boardWidth = 10, boardHeight = 10;
     const char* targetIP;
     unsigned int port;
-    bool runServer;
-    for(int i = 1; i < argc; i++)
+    for(int i = 1; i < argc; i++) // flag handling
     {
-        if(strcmp(argv[i], "-Ws") == 0)
+        if(strcmp(argv[i], "-Ws") == 0) // window size flag
         {
             windowWidth = std::stoi(argv[i+1]);
             windowHeight = std::stoi(argv[i+2]);
             i+=2;
         }
-        else if(strcmp(argv[i], "-h") == 0)
-            runServer = true;
-        else if(strcmp(argv[i], "-Bs") == 0)
+        else if(strcmp(argv[i], "-Bs") == 0) // board size flag
         {
             boardWidth = std::stoi(argv[i+1]);
             boardHeight = std::stoi(argv[i+2]);
