dc5e8781f8b52fac6fb29fd00ca34e4208e51ef7
Kamil <kazdancewicz@gmail.com>
2024-01-14 21:51:38 +0100

    Added help frame + visual

--- client.py
+++ client.py
@@ -6,78 +6,103 @@ from tkinter import messagebox, Label, Entry, simpledialog
 
 window_width = 960
 window_height = 540
-ip_address = "0"
+ip_address = "0.0.0.0"
 port = "2137"
 
+
 def launch():
-    #print(launch_arguments)
+    # print(launch_arguments)
     try:
         if platform.system() == "Windows":
-            #messagebox.showinfo("Windows!", "Uruchamiam grę na systemie Windows.")
-            p=subprocess.run(["okrety-delta.exe"] + launch_arguments, shell=True, check=False)
+            # messagebox.showinfo("Windows!", "Uruchamiam grę na systemie Windows.")
+            p = subprocess.run(["okrety-delta.exe"] + launch_arguments, shell=True, check=False)
             returnProcess = p.returncode
-            if(returnProcess==1):
+            if returnProcess == 1:
                 messagebox.showerror("Błąd", "Złe wejście")
-            elif(returnProcess==2):
+            elif returnProcess == 2:
                 messagebox.showerror("Błąd", "Nie udało się połączyć")
 
         else:
-            #messagebox.showinfo("Linux!", "Uruchamiam grę na systemie Linux.")
-            p=subprocess.run(["./okrety-delta"] + launch_arguments, shell=True, check=False)
+            # messagebox.showinfo("Linux!", "Uruchamiam grę na systemie Linux.")
+            p = subprocess.run(["./okrety-delta"] + launch_arguments, shell=True, check=False)
             returnProcess = p.returncode
-            if(returnProcess==1):
+            if returnProcess == 1:
                 messagebox.showerror("Błąd", "Złe wejście")
-            elif(returnProcess==2):
+            elif returnProcess == 2:
                 messagebox.showerror("Błąd", "Nie udało się połączyć")
 
     except subprocess.CalledProcessError as e:
         messagebox.showerror("Błąd", f"Wystąpił błąd podczas uruchamiania gry: {e}")
 
+
 def show_help():
     messagebox.showinfo("Pomoc", "Gra w statki to klasyczna rozgrywka strategiczna, w której gracze rozmieszczają "
-                                 "swoje statki na planszy i starają się trafić statki przeciwnika, wygrywając tę grę "
-                                 "zręcznościową i logiczną.\n\nLauncher gry oferuje kilka opcji, umożliwiając "
-                                 "dostosowanie doświadczenia rozgrywki. Możesz zmieniać rozmiar okna gry, przełączać "
+                                 "swoje statki na planszy i starają się trafić statki przeciwnika przez strategiczne "
+                                 "i logiczne ruchy podzielone na tury.\n\nLauncher gry oferuje kilka opcji, umożliwiając"
+                                 " dostosowanie doświadczenia rozgrywki. Możesz zmieniać rozmiar okna gry, przełączać "
                                  "między trybem pełnoekranowym a oknem, a także dostosowywać inne ustawienia, takie "
-                                 "jak adres IP hosta dla gry wieloosobowej.")
+                                 "jak adres IP hosta dla gry wieloosobowej. Dostępne jest również zapoznanie się z "
+                                 "elementami wizualnymi gry przed rozpoczęciem rozgrywki.")
+
 
 def toggle_fullscreen(arguments):
     # Toggle pełnego ekranu
-    if root.attributes("-fullscreen") == True:
+    if root.attributes("-fullscreen"):
         arguments.remove("-Fs")
     else:
         arguments.append("-Fs")
 
     root.attributes("-fullscreen", not root.attributes("-fullscreen"))
 
-def toggle_buttons():
+
+def toggle_option_buttons():
     # Ukryj/przywróć przyciski uruchamiania i pomocy
     start_button.pack_forget()
     help_button.pack_forget()
     options_button.pack_forget()
     exit_button.pack_forget()
 
-    back_button.pack(side=tk.LEFT, padx=10)
+    option_back_button.pack(side=tk.LEFT, padx=25)
 
     fullscreen_frame.pack(side=tk.LEFT, padx=25)
 
+
+def toggle_help_buttons():
+    # Ukryj/przywróć przyciski uruchamiania i pomocy
+    start_button.pack_forget()
+    help_button.pack_forget()
+    options_button.pack_forget()
+    exit_button.pack_forget()
+
+    help_frame.pack(side=tk.BOTTOM, pady=25)
+    gui_img_frame.pack(side=tk.LEFT, padx=25)
+    gui_desc_frame.pack(side=tk.LEFT, padx=0)
+
+
 def back_to_menu():
-    # Przywróć przyciski uruchamiania i pomocy, ukryj przyciski powrotu i pełnego ekranu
-    back_button.pack_forget()
+    # Przywróć przyciski menu głównego, ukryj gui pomocy/opcji
+    option_back_button.pack_forget()
     fullscreen_frame.pack_forget()
+
+    help_frame.pack_forget()
+    gui_img_frame.pack_forget()
+    gui_desc_frame.pack_forget()
 
     start_button.pack(pady=20)
     options_button.pack(pady=20)
     help_button.pack(pady=20)
     exit_button.pack(pady=20)
 
+
 def set_background():
     label1.config(image=bg)
     label1.place(x=-5, y=-5)
     label1.config(width=2560, height=1440)
 
+
 def change_button_color_hex(button, hex_color):
     button.config(bg=hex_color)
+
 
 def update_dimensions():
     # Funkcja wywoływana po wciśnięciu przycisku "Aktualizuj"
@@ -107,6 +132,7 @@ def get_ip_address():
     else:
         messagebox.showerror("Błąd", "Podany adres IP jest niepoprawny.")
 
+
 def is_valid_ip(ip):
     try:
         # Sprawdzenie, czy adres IP jest poprawny
@@ -115,8 +141,10 @@ def is_valid_ip(ip):
     except ipaddress.AddressValueError:
         return False
 
+
 def exit():
     root.destroy()
+
 
 if __name__ == "__main__":
     # Tworzenie głównego okna
@@ -155,10 +183,10 @@ if __name__ == "__main__":
     start_button = tk.Button(root, text="Uruchom grę", command=lambda: (get_ip_address()), height=3, width=15)
     start_button.pack(pady=20)
 
-    options_button = tk.Button(root, text="Opcje", command=toggle_buttons, height=3, width=15)
+    options_button = tk.Button(root, text="Opcje", command=toggle_option_buttons, height=3, width=15)
     options_button.pack(pady=20)
 
-    help_button = tk.Button(root, text="Pomoc", command=show_help, height=3, width=15)
+    help_button = tk.Button(root, text="Pomoc", command=toggle_help_buttons, height=3, width=15)
     help_button.pack(pady=20)
 
     exit_button = tk.Button(root, text="Wyjście", command=exit, height=3, width=15)
@@ -169,29 +197,77 @@ if __name__ == "__main__":
     change_button_color_hex(help_button, "#67B7D1")
     change_button_color_hex(exit_button, "#67B7D1")
 
-    # Przyciski powrotu i pełnego ekranu (na razie ukryte)
-    back_button = tk.Button(root, text="Powrót", command=back_to_menu, height=3, width=15)
+    # Przyciski powrotu i pełnego ekranu w ekranie opcji
+    option_back_button = tk.Button(root, text="Powrót", command=back_to_menu, height=3, width=15)
 
     fullscreen_frame = tk.Frame(root, bg="#67B7D1")
-    fullscreen_button = tk.Button(fullscreen_frame, text="Pełny ekran", command=lambda: toggle_fullscreen(launch_arguments), height=3, width=15)
-    fullscreen_button.pack()
+    fullscreen_button = tk.Button(fullscreen_frame, text="Pełny ekran",
+                                  command=lambda: toggle_fullscreen(launch_arguments), height=3, width=15, fg="#000040")
+    fullscreen_button.pack(pady=5)
 
     change_button_color_hex(fullscreen_button, "#ADD8E6")
-    change_button_color_hex(back_button, "#ADD8E6")
+    change_button_color_hex(option_back_button, "#ADD8E6")
 
     # Pola Entry dla szerokości i wysokości okna
-    label_text = tk.Label(fullscreen_frame, text="Szerokość okna gry", fg="#3E6B89")
+    label_text = tk.Label(fullscreen_frame, text="Szerokość okna gry", fg="#00004d", bg="#3ba1c2")
     label_text.pack(padx=10)
     width_entry = Entry(fullscreen_frame, width=10, textvariable=width_var)
     width_entry.pack()
 
-    label_text = tk.Label(fullscreen_frame, text="Wysokość okna gry", fg="#3E6B89")
+    label_text = tk.Label(fullscreen_frame, text="Wysokość okna gry", fg="#00004d", bg="#3ba1c2")
     label_text.pack(padx=10)
     height_entry = Entry(fullscreen_frame, width=10, textvariable=height_var)
     height_entry.pack()
 
-    update_button = tk.Button(fullscreen_frame, text="Aktualizuj", command=update_dimensions, height=2, width=15, fg="#3E6B89")
-    update_button.pack(pady=10)
+    update_button = tk.Button(fullscreen_frame, text="Aktualizuj", command=update_dimensions, height=2, width=15,
+                              fg="#000040", bg="#ADD8E6")
+    update_button.pack(pady=5)
+
+    # Przycisk powrotu i opisu w ekranie pomocy
+    help_frame = tk.Frame(root, bg="#67B7D1")
+    help_back_button = tk.Button(help_frame, text="Powrót", command=back_to_menu, height=3, width=15)
+    help_back_button.pack(side=tk.LEFT, padx=5, pady=5)
+    desc_button = tk.Button(help_frame, text="Opis", command=show_help, height=3, width=15)
+    desc_button.pack(side=tk.LEFT, padx=5, pady=5)
+
+    change_button_color_hex(help_back_button, "#ADD8E6")
+    change_button_color_hex(desc_button, "#ADD8E6")
+
+    # Opisy gui
+    gui_img_frame = tk.Frame(root, bg="#67B7D1")
+    gui_desc_frame = tk.Frame(root, bg="#67B7D1")
+
+    blue_square_img = tk.PhotoImage(file="assets/blueSquareMarker.png")
+    black_square_img = tk.PhotoImage(file="assets/squareMarker.png")
+    red_x_img = tk.PhotoImage(file="assets/crossMarker.png")
+    blue_circle_img = tk.PhotoImage(file="assets/circleMarker.png")
+    green_check_img = tk.PhotoImage(file="assets/checkMarker.png")
+
+    # Dodawanie obrazów
+    blue_square_label = tk.Label(gui_img_frame, image=blue_square_img, bg="#67B7D1")
+    blue_square_label.pack(side=tk.TOP, pady=5)
+    black_square_label = tk.Label(gui_img_frame, image=black_square_img, bg="#67B7D1")
+    black_square_label.pack(side=tk.TOP, pady=5)
+    red_x_label = tk.Label(gui_img_frame, image=red_x_img, bg="#67B7D1")
+    red_x_label.pack(side=tk.TOP, pady=5)
+    blue_circle_label = tk.Label(gui_img_frame, image=blue_circle_img, bg="#67B7D1")
+    blue_circle_label.pack(side=tk.TOP, pady=5)
+    green_check_label = tk.Label(gui_img_frame, image=green_check_img, bg="#67B7D1")
+    green_check_label.pack(side=tk.TOP, pady=5)
+
+    # Dodawanie opisów obrazów
+    label_text = tk.Label(gui_desc_frame, text="Niebieski kwadrat oznacza stawiany statek", fg="#00004d", bg="#3ba1c2")
+    label_text.pack(side=tk.TOP, pady=13)
+    label_text = tk.Label(gui_desc_frame, text="Czarny kwadrat oznacza postawiony już statek", fg="#00004d",
+                          bg="#3ba1c2")
+    label_text.pack(side=tk.TOP, pady=14)
+    label_text = tk.Label(gui_desc_frame, text="Czerwony X oznacza trafienie", fg="#00004d", bg="#3ba1c2")
+    label_text.pack(side=tk.TOP, pady=14)
+    label_text = tk.Label(gui_desc_frame, text="Niebieskie koło oznacza nietrafiony strzał", fg="#00004d", bg="#3ba1c2")
+    label_text.pack(side=tk.TOP, pady=14)
+    label_text = tk.Label(gui_desc_frame, text="Zielony checkmark służy do potwierdzania decyzji", fg="#00004d",
+                          bg="#3ba1c2")
+    label_text.pack(side=tk.TOP, pady=13)
 
     # Uruchomienie głównej pętli programu
     root.mainloop()
