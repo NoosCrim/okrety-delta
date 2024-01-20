cca6efd8851968f0f59896fe4a7ff56ab23747f7
Milosz <85848694+Msaters@users.noreply.github.com>
2024-01-20 02:07:07 +0100

    Update serverMain.cpp

--- serverMain.cpp
+++ serverMain.cpp
@@ -1,24 +1,14 @@
-#include "gameGUI.hpp"
-int main(int argc, const char *argv[])
-{
-    /// flag handling
-    unsigned int port = 0;
-    for(int i = 1; i < argc; i++) // flag handling
-    {
-        if(strcmp(argv[i], "-P") == 0) // host port flag
-        {
-            port = std::stoi(argv[i+1]);
-            i+=1;
-        }
-    }
-    if(port == 0)
-    {
-        std::cerr << "Please, provide port for the server" << std::endl;
-        return 1;
-    }
-    /// end of flag handling
+#include "networking/server.hpp"
 
-    AVE::Init();
-    AVE::DeInit();
+int main() {
+    try {
+        asio::io_context ioContext;
+        Server server(ioContext, SERVER_PORT, STARTING_PLAYER_NUMBER);
+        ioContext.run();
+    } catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
     return 0;
 }
+
